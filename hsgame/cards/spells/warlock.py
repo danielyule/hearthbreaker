d9f9708ad0c1757a0370e75d7a29a1033c6fbb26
randomflyingtaco <randomflyingtaco@yahoo.com>
2014-06-18 06:36:09 -0500

    Added Mortal Coil, Shadow Bolt, and Hellfire

--- hsgame/cards/spells/warlock.py
+++ hsgame/cards/spells/warlock.py
@@ -9,6 +9,29 @@ class MortalCoil(Card):
         super().__init__("Mortal Coil", 1, CHARACTER_CLASS.WARLOCK, CARD_RARITY.FREE, hsgame.targeting.find_minion_spell_target)
 
         def use(self, player, game):
-            target.spell_damage(1 + player.spell_power, wrath)
-            if minion.health <= 1 +player.spell_power:
-              player.draw()
+            target.spell_damage(1 + player.spell_power, self)
+            if minion.health <= 1 + player.spell_power:
+                player.draw()
+
+class Hellfire(Card):
+    def __init__(self):
+        super().__init__("Hellfire", 4, CHARACTER_CLASS.WARLOCK, CARD_RARITY.FREE)
+
+    def use(self, player, game):
+        super().use(player, game)
+        targets = game.other_player.minions.copy()
+        targets.extend(game.current_player.minions)
+        targets.append(game.other_player.hero)
+        targets.append(game.current_player.hero)
+        for minion in targets:
+            minion.spell_damage(3 + player.spell_power, self)
+
+class ShadowBolt(Card):
+    def __init__(self):
+        super().__init__("Shadow Bolt", 3, CHARACTER_CLASS.WARLOCK, CARD_RARITY.FREE,
+                         hsgame.targeting.find_minion_spell_target)
+
+    def use(self, player, game):
+        super().use(player, game)
+        self.target.spell_damage(4 + player.spell_power, self)
+
