f228b0d76a5c619e45d40d4d0da12059cb2668e9
randomflyingtaco <randomflyingtaco@yahoo.com>
2014-06-18 06:09:02 -0500

    Create warlock.py

---
+++ hsgame/cards/minions/warlock.py
@@ -0,0 +1,8 @@
+import hsgame.targeting
+from hsgame.constants import CHARACTER_CLASS, CARD_RARITY, MINION_TYPE
+from hsgame.game_objects import MinionCard, Minion, Card
+#from hsgame.cards.battlecries import 
+
+__author__ = 'randomflyingtaco'
+#let the train wreck begin
+
