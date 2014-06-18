5b128b306f1f50beb4ba9971029c561154bcc78c
randomflyingtaco <randomflyingtaco@yahoo.com>
2014-06-18 04:36:38 -0500

    Fixed Cabal being unable to target magic immune

--- hsgame/cards/minions/priest.py
+++ hsgame/cards/minions/priest.py
@@ -8,7 +8,7 @@ __author__ = 'Daniel'
 
 class CabalShadowPriest(MinionCard):
     def __init__(self):
-        super().__init__("Cabal Shadow Priest", 6, CHARACTER_CLASS.PRIEST, CARD_RARITY.EPIC, hsgame.targeting.find_enemy_minion_battlecry_target, lambda target: target.attack_power <= 2 and target.spell_targetable())
+        super().__init__("Cabal Shadow Priest", 6, CHARACTER_CLASS.PRIEST, CARD_RARITY.EPIC, hsgame.targeting.find_enemy_minion_battlecry_target, lambda target: target.attack_power <= 2)
 
     def create_minion(self, player):
         return Minion(4, 5, battlecry=take_control_of_minion)
