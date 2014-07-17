a61116eeacd0f260624a80e6e86638f3333deb3f
Daniel Yule <daniel.yule@gmail.com>
2014-07-16 23:02:57 -0300

    Implemented some interactions with the game

--- hsgame/ui/game_printer.py
+++ hsgame/ui/game_printer.py
@@ -26,68 +26,7 @@ def abbreviate(card_name):
     return card_abbreviations.get(card_name, card_name)
 
 
-def draw_minion(minion, window, y, x):
-    status_array = []
-    if minion.can_attack():
-        status_array.append("*")
-        color = curses.color_pair(2)
-    else:
-        color = curses.color_pair(1)
-    if "attack" in minion.events:
-        status_array.append("a")
-    if "turn_start" in minion.events:
-        status_array.append("b")
-    if minion.charge:
-        status_array.append("c")
-    if minion.deathrattle is not None:
-        status_array.append("d")
-    if minion.enraged:
-        status_array.append("e")
-    if minion.frozen:
-        status_array.append("f")
-    if minion.immune:
-        status_array.append("i")
-    if minion.stealth:
-        status_array.append("s")
-    if minion.taunt:
-        status_array.append("t")
-    if minion.exhausted:
-        status_array.append("z")
 
-    name = abbreviate(minion.card.name)[:9]
-    status = ''.join(status_array)
-    power_line = "({0}) ({1})".format(minion.calculate_attack(), minion.health)
-    window.addstr(y + 0, x, "{0:^9}".format(name), color)
-    window.addstr(y + 1, x, "{0:^9}".format(power_line), color)
-    window.addstr(y + 2, x, "{0:^9}".format(status), color)
-
-
-def draw_card(card, player, window, y, x):
-    if card.can_use(player, player.game):
-        status = "*"
-        color = curses.color_pair(2)
-    else:
-        status = " "
-        color = curses.color_pair(1)
-    name = card.name[:15]
-    window.addstr(y + 0, x, " {0:>2} mana ({1})   ".format(card.mana_cost(player), status), color)
-    window.addstr(y + 1, x, "{0:^15}".format(name), color)
-
-
-def draw_hero(hero, window, x, y):
-    if hero.weapon is not None:
-        weapon_power = "({0}) ({1})".format(hero.weapon.base_attack, hero.weapon.durability)
-        window.addstr(y, x, "{0:^20}".format(hero.weapon.card.name))
-        window.addstr(y + 1, x, "{0:^20}".format(weapon_power))
-
-    hero_power = "({0}) ({1}) -- {2}/{3} mana".format(hero.calculate_attack(), hero.health,
-                                                  hero.player.mana, hero.player.max_mana)
-    window.addstr(y, x + 20, "{0:^20}".format(CHARACTER_CLASS.to_str(hero.character_class)))
-    window.addstr(y + 1, x + 20, "{0:^20}".format(hero_power))
-
-    window.addstr(y, x + 40, "{0:^20}".format("Hero Power"))
-    if hero.power.can_use():
-        window.addstr(y + 1, x + 40, "{0:^20}".format("*"))
 
 
 def game_to_string(game):
@@ -105,6 +44,8 @@ class GameRender:
 
         curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
         curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_WHITE)
+        curses.init_pair(3, curses.COLOR_BLACK, curses.COLOR_BLUE)
+        curses.init_pair(4, curses.COLOR_BLACK, curses.COLOR_YELLOW)
 
         self.top_minion_window = window.derwin(3, 80, 4, 0)
         self.bottom_minion_window = window.derwin(3, 80, 8, 0)
@@ -112,6 +53,94 @@ class GameRender:
         self.card_window = window.derwin(5, 80, 16, 0)
         self.window = window
         self.game = game
+        self.targets = None
+        self.selected_target = None
+
+    def draw_minion(self, minion, window, y, x):
+        status_array = []
+        color = curses.color_pair(0)
+        if minion.can_attack():
+            status_array.append("*")
+            if not self.targets:
+                color = curses.color_pair(2)
+        else:
+            if not self.targets:
+                color = curses.color_pair(1)
+        if "attack" in minion.events:
+            status_array.append("a")
+        if "turn_start" in minion.events:
+            status_array.append("b")
+        if minion.charge:
+            status_array.append("c")
+        if minion.deathrattle is not None:
+            status_array.append("d")
+        if minion.enraged:
+            status_array.append("e")
+        if minion.frozen:
+            status_array.append("f")
+        if minion.immune:
+            status_array.append("i")
+        if minion.stealth:
+            status_array.append("s")
+        if minion.taunt:
+            status_array.append("t")
+        if minion.exhausted:
+            status_array.append("z")
+
+        if self.targets:
+            if minion is self.selected_target:
+                color = curses.color_pair(4)
+            elif minion in self.targets:
+                color = curses.color_pair(3)
+
+
+        name = abbreviate(minion.card.name)[:9]
+        status = ''.join(status_array)
+        power_line = "({0}) ({1})".format(minion.calculate_attack(), minion.health)
+        window.addstr(y + 0, x, "{0:^9}".format(name), color)
+        window.addstr(y + 1, x, "{0:^9}".format(power_line), color)
+        window.addstr(y + 2, x, "{0:^9}".format(status), color)
+
+    def draw_card(self, card, player, window, y, x):
+        color = curses.color_pair(0)
+        if card.can_use(player, player.game):
+            status = "*"
+            if not self.targets:
+                color = curses.color_pair(2)
+        else:
+            status = " "
+            if not self.targets:
+                color = curses.color_pair(1)
+        if self.targets:
+            if card is self.selected_target:
+                color = curses.color_pair(4)
+            elif card in self.targets:
+                color = curses.color_pair(3)
+
+        name = card.name[:15]
+        window.addstr(y + 0, x, " {0:>2} mana ({1})   ".format(card.mana_cost(player), status), color)
+        window.addstr(y + 1, x, "{0:^15}".format(name), color)
+
+    def draw_hero(self, hero, window, x, y):
+        color = curses.color_pair(0)
+        if self.targets:
+            if hero is self.selected_target:
+                color = curses.color_pair(4)
+            elif hero in self.targets:
+                color = curses.color_pair(3)
+        if hero.weapon is not None:
+            weapon_power = "({0}) ({1})".format(hero.weapon.base_attack, hero.weapon.durability)
+            window.addstr(y, x, "{0:^20}".format(hero.weapon.card))
+            window.addstr(y + 1, x, "{0:^20}".format(weapon_power))
+
+        hero_power = "({0}) ({1}+{4}) -- {2}/{3}".format(hero.calculate_attack(), hero.health,
+                                                         hero.player.mana, hero.player.max_mana, hero.armour)
+        window.addstr(y, x + 20, "{0:^20}".format(CHARACTER_CLASS.to_str(hero.character_class)), color)
+        window.addstr(y + 1, x + 20, "{0:^20}".format(hero_power), color)
+
+        window.addstr(y, x + 40, "{0:^20}".format("Hero Power"))
+        if hero.power.can_use():
+            window.addstr(y + 1, x + 40, "{0:^20}".format("*"))
 
     def draw_game(self):
         self.window.clear()
@@ -123,14 +152,14 @@ class GameRender:
             l_offset = int((80 - 10 * len(minions)) / 2)
             index = 0
             for minion in minions:
-                draw_minion(minion, window, 0, l_offset + index * 10)
+                self.draw_minion(minion, window, 0, l_offset + index * 10)
                 index += 1
 
         def draw_cards(cards, player, window, y):
             l_offset = int((80 - 16 * len(cards)) / 2)
             index = 0
             for card in cards:
-                draw_card(card, player, window, y, l_offset + index * 16)
+                self.draw_card(card, player, window, y, l_offset + index * 16)
                 index += 1
 
         curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
@@ -142,8 +171,8 @@ class GameRender:
         draw_cards(self.bottom_player.hand[:5], self.bottom_player, self.card_window, 0)
         draw_cards(self.bottom_player.hand[5:], self.bottom_player, self.card_window, 3)
 
-        draw_hero(self.top_player.hero, self.window, 10, 0)
-        draw_hero(self.bottom_player.hero, self.window, 10, 12)
+        self.draw_hero(self.top_player.hero, self.window, 10, 0)
+        self.draw_hero(self.bottom_player.hero, self.window, 10, 12)
         self.window.refresh()
         self.bottom_minion_window.refresh()
         self.top_minion_window.refresh()
--- hsgame/ui/text_runner.py
+++ hsgame/ui/text_runner.py
@@ -1,13 +1,13 @@
+import copy
 import curses
 import curses.textpad
+import sys
 from hsgame.constants import CHARACTER_CLASS
 from hsgame.game_objects import Deck, Game
 from hsgame.cards import *
 from hsgame.ui.game_printer import GameRender
 from tests.testing_agents import SpellTestingAgent
 from tests.testing_utils import StackedDeck
-
-
 
 def render_game(stdscr):
     class TextAgent:
@@ -24,12 +24,98 @@ def render_game(stdscr):
             self.window.refresh()
             self.text_window.clear()
             self.text_window.refresh()
-            return self.text_box.edit()
-
+            text = self.text_box.edit()
+            return text.strip().lower()
 
         def do_turn(self, player):
             renderer.draw_game()
-            self.prompt("What now?")
+            index = 0
+            action = self.prompt("What now? (attack, play, power, quit, end)")
+            while not (action == "quit" or action == "end"):
+                if action == "play":
+                    card = self.choose_card()
+                    if card is not None:
+                        self.game.play_card(card)
+                elif action == "attack":
+                    attacker = self.choose_attacker()
+                    if attacker is not None:
+                        attacker.attack()
+                elif action == "power":
+                    if player.hero.power.can_use():
+                        player.hero.power.use()
+                index += 1
+                renderer.draw_game()
+                action = self.prompt("What now? (attack, play, power, quit, end)")
+            if action == "quit":
+                sys.exit(0)
+
+        def choose_card(self):
+            filtered_cards = [card for card in filter(lambda card: card.can_use(self.game.current_player, self.game),
+                                                      self.game.current_player.hand)]
+            if len(filtered_cards) is 0:
+                return None
+            renderer.targets = filtered_cards
+            renderer.selected_target = renderer.targets[0]
+            renderer.draw_game()
+            self.window.addstr(0, 0, "Choose Card")
+            self.window.refresh()
+            ch = 0
+            index = 0
+            while ch != 10 and ch != 27:
+                ch = self.window.getch()
+
+                if ch == curses.KEY_LEFT:
+                    index -= 1
+                    if index < 0:
+                        index = len(renderer.targets) - 1
+                if ch == curses.KEY_RIGHT:
+                    index += 1
+                    if index == len(renderer.targets):
+                        index = 0
+                renderer.selected_target = renderer.targets[index]
+                renderer.draw_game()
+                self.window.addstr(0, 0, "Choose Card")
+                self.window.refresh()
+            renderer.targets = None
+            if ch == 27:
+                return None
+
+            return renderer.selected_target
+
+        def choose_attacker(self):
+            filtered_attackers = [minion for minion in filter(lambda minion: minion.can_attack(),
+                                                              self.game.current_player.minions)]
+            if self.game.current_player.hero.can_attack():
+                filtered_attackers.append(self.game.current_player.hero)
+            if len(filtered_attackers) is 0:
+                return None
+            renderer.targets = filtered_attackers
+            renderer.selected_target = renderer.targets[0]
+            renderer.draw_game()
+            self.window.addstr(0, 0, "Choose attacker")
+            self.window.refresh()
+            ch = 0
+            index = 0
+            while ch != 10 and ch != 27:
+                ch = self.window.getch()
+                self.window.addstr(0, 0, "{0}".format(ch))
+                self.window.refresh()
+                if ch == curses.KEY_LEFT:
+                    index -= 1
+                    if index < 0:
+                        index = len(renderer.targets) - 1
+                if ch == curses.KEY_RIGHT:
+                    index += 1
+                    if index == len(renderer.targets):
+                        index = 0
+                renderer.selected_target = renderer.targets[index]
+                renderer.draw_game()
+                self.window.refresh()
+            renderer.targets = None
+            if ch == 27:
+                return None
+
+            return renderer.selected_target
 
         def do_card_check(self, cards):
             return [True, True, True, True]
@@ -38,7 +124,34 @@ def render_game(stdscr):
             self.game = game
 
         def choose_target(self, targets):
-            return targets[0]
+
+            if len(targets) is 0:
+                return None
+            renderer.targets = targets
+            renderer.selected_target = renderer.targets[0]
+            renderer.draw_game()
+            self.window.addstr(0, 0, "Choose target")
+            self.window.refresh()
+            ch = 0
+            index = 0
+            while ch != 10 and ch != 27:
+                ch = self.window.getch()
+                if ch == curses.KEY_LEFT:
+                    index -= 1
+                    if index < 0:
+                        index = len(renderer.targets) - 1
+                if ch == curses.KEY_RIGHT:
+                    index += 1
+                    if index == len(renderer.targets):
+                        index = 0
+                renderer.selected_target = renderer.targets[index]
+                renderer.draw_game()
+                self.window.refresh()
+            renderer.targets = None
+            if ch == 27:
+                return None
+
+            return renderer.selected_target
 
         def choose_index(self, card):
             return 0
@@ -56,15 +169,16 @@ def render_game(stdscr):
     #
     # stdscr.refresh()
     # stdscr.getkey()
-    prompt_window = stdscr.derwin(1, 80, 25, 0)
-    text_window = stdscr.derwin(1, 80, 26, 0)
+    prompt_window = stdscr.derwin(1, 80, 23, 0)
+    text_window = stdscr.derwin(1, 80, 24, 0)
 
     deck1 = StackedDeck([StonetuskBoar(), BluegillWarrior(), Wolfrider()], CHARACTER_CLASS.DRUID)
     deck2 = StackedDeck([FrostwolfGrunt(), GoldshireFootman(), IronfurGrizzly()], CHARACTER_CLASS.MAGE)
     game = Game([deck1, deck2], [TextAgent(stdscr, prompt_window, text_window), SpellTestingAgent()])
-    game.players[0].agent.set_game(game)
-    game.players[1].agent.set_game(game)
-    renderer = GameRender(stdscr, game, game.players[0])
+    if isinstance(game.players[0].agent, TextAgent):
+        renderer = GameRender(stdscr, game, game.players[0])
+    else:
+        renderer = GameRender(stdscr, game, game.players[1])
     game.start()
 
 if __name__ == "__main__":
