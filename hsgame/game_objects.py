9286bd8f18089fb79a4b056c556780cd021de5fc
Daniel Yule <daniel.yule@gmail.com>
2014-07-16 23:04:54 -0300

    Fix for auras, so they do not affect heroes.

--- hsgame/game_objects.py
+++ hsgame/game_objects.py
@@ -321,13 +321,10 @@ class Character(Bindable, metaclass=abc.ABCMeta):
     def calculate_attack(self):
         """
         Calculates the amount of attack this :class:`Character` has, including the base attack, any temporary attack
-        bonuses for this turn and any aura effects
+        bonuses for this turn
         """
-        aura_attack = 0
-        for aura in self.player.auras:
-            if aura.filter(self):
-                aura_attack += aura.attack
-        return self.base_attack + self.temp_attack + aura_attack
+
+        return self.base_attack + self.temp_attack
 
     def calculate_max_health(self):
         """
@@ -731,6 +728,17 @@ class Minion(Character):
         self.health += self.calculate_max_health() - self.base_health
         self.game.trigger("minion_added", self)
         self.trigger("added_to_board", self, index)
+
+    def calculate_attack(self):
+        """
+        Calculates the amount of attack this :class:`Minion` has, including the base attack, any temporary attack
+        bonuses for this turn and any aura effects
+        """
+        aura_attack = 0
+        for aura in self.player.auras:
+            if aura.filter(self):
+                aura_attack += aura.attack
+        return super().calculate_attack() + aura_attack
 
     def remove_from_board(self):
         self.silence()  # Neutralize all possible effects
