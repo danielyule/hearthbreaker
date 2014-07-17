56028840c08eed1bfe88b6e99e787dde70aab276
Ragowit <ragowit@grh.se>
2014-07-17 17:36:07 +0200

    Implemented Grommash Hellscream

--- hsgame/cards/minions/__init__.py
+++ hsgame/cards/minions/__init__.py
@@ -217,5 +217,6 @@ from hsgame.cards.minions.warrior import (
     ArathiWeaponsmith,
     Armorsmith,
     CruelTaskmaster,
-    FrothingBerserker
+    FrothingBerserker,
+    GrommashHellscream
 )
